ed72f2483bedfa35c7c822d3598387c73c2da5c1
root <root@worker3-pre.ylly.co>
2016-11-02 05:19:43 +0000

    change byteformat to byteformat==0.2a

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="http://docs.bakthat.io",
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
     long_description=read('README.rst'),
-    install_requires=["aaargh", "boto", "pycrypto", "beefish", "grandfatherson", "peewee", "byteformat==0.2a0", "pyyaml", "sh", "requests", "events"],
+    install_requires=["aaargh", "boto", "pycrypto", "beefish", "grandfatherson", "peewee", "byteformat==0.2a", "pyyaml", "sh", "requests", "events"],
     entry_points={'console_scripts': ["bakthat = bakthat:main"]},
     classifiers=[
         "Development Status :: 4 - Beta",
